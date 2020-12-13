6e5713c872fc60c91ff080acc60e3f49883cea87
Alisher <alishergoold1989@gmail.com>
2020-12-13 13:47:19 +0500

    Yangi fayl qo'shildi
    Test va xaudixo fayllarni qo'shdik

---
+++ xaudixo.py
@@ -0,0 +1,13 @@
+'''# perevod stroki \n
+print('Salom\ndunyo')
+#Salom
+#dunyo'''
+
+# konkotenatsiya strok == satrlarni qo'shish
+name = 'Alisher'
+age = 31
+print('Salom ' + name + '!')
+print('Men ' + str(31) + ' yoshdaman!!') 
+# string bilan number tipidagi o'zgaruvchilarni bi biriga qo'shib bo'lmaydi. biz
+# numberni stringga almashtirdik.
+#Salom Alisher
